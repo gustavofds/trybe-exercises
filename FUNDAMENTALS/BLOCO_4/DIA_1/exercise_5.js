fb2b238c6dad42d6250540a6852db84a249c9d7f
Gustavo Sousa <gustavohl98@gmail.com>
2021-02-24 20:18:53 -0300

    Eercicio 5

---
+++ FUNDAMENTALS/BLOCO_4/DIA_1/exercise_5.js
@@ -0,0 +1,10 @@
+// Exercício 5 - Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário.
+let a = 45;
+let b = 45;
+let c = 90;
+
+if (a + b + c === 180) {
+  console.log(true);
+} else {
+  console.log(false);
+}
