72c8bff0d330ca53f2382ada8935a0c938a521b8
Gustavo Sousa <gustavohl98@gmail.com>
2021-02-24 19:20:06 -0300

    Reorganiza exercicios criando 1 arq para cada

---
+++ FUNDAMENTALS/BLOCO_4/DIA_1/exercise_1.js
@@ -0,0 +1,9 @@
+// Exercício 1
+let a = 10;
+let b = 5;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
---
+++ FUNDAMENTALS/BLOCO_4/DIA_1/exercise_2.js
@@ -0,0 +1,9 @@
+// Exercício 2
+let a = 55;
+let b = 4;
+
+if (a > b) {
+  console.log("O número 'a' é maior");
+} else if (b > a) {
+  console.log("O número 'b' é maior");
+}
