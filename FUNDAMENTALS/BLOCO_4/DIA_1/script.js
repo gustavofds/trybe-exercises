61a37d639084528c7c06b4a6254ba485c776ce0c
Gustavo Sousa <gustavohl98@gmail.com>
2021-02-24 18:58:57 -0300

    Exercicio 2

--- FUNDAMENTALS/BLOCO_4/DIA_1/script.js
+++ FUNDAMENTALS/BLOCO_4/DIA_1/script.js
@@ -1,3 +1,4 @@
+// Exercício 1
 let a = 10;
 let b = 5;
 
@@ -6,3 +7,13 @@ let subtracao = a - b;
 let multiplicacao = a * b;
 let divisao = a / b;
 let modulo = a % b;
+
+// Exercício 2
+let c = 55;
+let d = 4;
+
+if (c > d) {
+  console.log("O número 'c' é maior");
+} else if (d > c) {
+  console.log("O número 'd' é maior");
+}
