1d06a3cc246db8488b96ace1324d6768c27adf3f
Gustavo Sousa <gustavohl98@gmail.com>
2021-02-25 18:40:03 -0300

    Exercise 2 e correcao no 1

--- FUNDAMENTALS/BLOCO_4/DIA_2/exercise_2.js
+++ FUNDAMENTALS/BLOCO_4/DIA_2/exercise_2.js
@@ -1,6 +1,9 @@
+// Exercise 2
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Exercise 1
+let soma = 0;
 for (let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
+  soma += numbers[index];
 }
+
+console.log("Soma: ", soma);
