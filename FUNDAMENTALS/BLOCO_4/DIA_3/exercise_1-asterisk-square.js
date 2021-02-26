2c5560f0add4a2c18e6707fe2495d19a3b1af775
Gustavo Sousa <gustavohl98@gmail.com>
2021-02-26 18:49:34 -0300

    Initial commit - Exercise 1

---
+++ FUNDAMENTALS/BLOCO_4/DIA_3/exercise_1-asterisk-square.js
@@ -0,0 +1,11 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+let n = 5;
+
+for (let index = 1; index <= n; index += 1) {
+  let row = "";
+  for (let indexColumn = 1; indexColumn <= n; indexColumn += 1) {
+    row += "*";
+  }
+  console.log(row);
+}
