fd4ed3d316577013b79e59b63d695126ca9e761a
Gustavo Sousa <gustavohl98@gmail.com>
2021-02-26 18:51:53 -0300

    Exercise 2

---
+++ FUNDAMENTALS/BLOCO_4/DIA_3/exercise_2-rectangle-triangle.js
@@ -0,0 +1,11 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
+
+for (let index = 1; index <= n; index += 1) {
+  let row = "";
+  for (let indexColumn = 1; indexColumn <= index; indexColumn += 1) {
+    row += "*";
+  }
+  console.log(row);
+}
