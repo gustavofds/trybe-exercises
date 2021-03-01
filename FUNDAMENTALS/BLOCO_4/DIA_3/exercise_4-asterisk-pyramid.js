076443494adc2c75c8e00ab8dc2df228c976ff7f
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-01 07:50:32 -0300

    Exercise 4 - Describing exercise

--- FUNDAMENTALS/BLOCO_4/DIA_3/exercise_4-asterisk-pyramid.js
+++ FUNDAMENTALS/BLOCO_4/DIA_3/exercise_4-asterisk-pyramid.js
@@ -1,9 +1,35 @@
 //4- Depois, faça uma pirâmide com n asteriscos de base:
 /*
- n = 5
+n = 3
+ *     (1 espaço + 1 asterisco)
+***    (0 espaços + 3 asteriscos)
 
+n = 5
   *    (2 espaços + 1 asterisco)
  ***   (1 espaço  + 3 asteriscos)
 *****  (0 espaços + 5 asteriscos)
 
+n = 7
+   *     (3 espaços + 1 asterisco)
+  ***    (2 espaços + 3 asterisco)
+ *****   (1 espaço + 5 asterisco)
+*******  (0 espaços + 7 asterisco)
+
+n = 9
+    *      (4 espaços + 1 asterisco)
+   ***     (3 espaços + 3 asterisco)
+  *****    (2 espaços + 5 asterisco)
+ *******   (1 espaços + 7 asterisco)
+*********  (0 espaços + 9 asterisco)
+
+Nº de rows === Nº de números ímpares descendo até o 1
+Math.ceil(n/2)
+
+Nº máximo de espaços(1ª row) === Math.trunc(n/2)
+
+Espaços descendem 1 a 1 até chegar em 0 na última row
  */
+
+console.log("  *");
+console.log(" ***");
+console.log("*****");
