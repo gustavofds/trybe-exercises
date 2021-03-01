dc2b5aebe756c16c9805dd362201c416dac5bb64
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-01 10:35:02 -0300

    Bonus exercise 5

---
+++ FUNDAMENTALS/BLOCO_4/DIA_3/bonus_exercise_5-pyramid-empty-inside.js
@@ -0,0 +1,35 @@
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+/*
+n = 7
+
+   *
+  * *
+ *   *
+*******
+ */
+
+const n = 7;
+
+const maxSpaces = Math.trunc(n / 2);
+console.log("maxSpaces: ", maxSpaces);
+
+// Max spaces string
+let spacesRow = "";
+for (let indexSpaces = maxSpaces; indexSpaces >= 0; indexSpaces -= 1) {
+  spacesRow += " ";
+}
+
+for (let index = 1; index <= n; index += 2) {
+  let row = "";
+
+  for (let indexAsterisks = 1; indexAsterisks <= index; indexAsterisks += 1) {
+    if (indexAsterisks === 1 || indexAsterisks === index || index === n) {
+      row += "*";
+    } else {
+      row += " ";
+    }
+  }
+
+  spacesRow = spacesRow.replace(spacesRow[spacesRow.length - 1], "");
+  console.log(spacesRow + row);
+}
