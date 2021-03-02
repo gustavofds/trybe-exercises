d6bd6e756d13b83b6e728dd9f3a8305cdfafd1ae
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-02 08:25:10 -0300

    Part II - exercise 2 + exercise 1 correction

--- FUNDAMENTALS/BLOCO_4/DIA_4/exercises_part_2.js
+++ FUNDAMENTALS/BLOCO_4/DIA_4/exercises_part_2.js
@@ -4,13 +4,28 @@ function isPalindrome(word) {
   for (let index = 0; index < word.length; index += 1) {
     if (word[index] !== word[word.length - 1 - index]) {
       return false;
-    } else {
-      return true;
     }
   }
+
+  return true;
 }
 
 console.log(isPalindrome("desenvolvimento"));
 console.log(isPalindrome("arara"));
 console.log(isPalindrome("renner"));
-console.log(isPalindrome("gustavo"));
+console.log(isPalindrome("gustavg"));
+
+function higherIndex(arr) {
+  let higherIndex = 0;
+
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] > arr[higherIndex]) {
+      higherIndex = index;
+    }
+  }
+
+  return higherIndex;
+}
+
+console.log(higherIndex([2, 3, 6, 7, 10, 1]));
+console.log(higherIndex([2, 3, 6, 555, 7, 10]));
