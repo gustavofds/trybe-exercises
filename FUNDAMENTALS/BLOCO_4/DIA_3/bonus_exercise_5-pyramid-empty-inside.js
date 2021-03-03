889fd87eba4e180805540dcfc12114d3fd87c7b1
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-03 07:50:57 -0300

    Remove unecessary console.logs and improve code of DAY 3 pyramid exercises

--- FUNDAMENTALS/BLOCO_4/DIA_3/bonus_exercise_5-pyramid-empty-inside.js
+++ FUNDAMENTALS/BLOCO_4/DIA_3/bonus_exercise_5-pyramid-empty-inside.js
@@ -11,11 +11,10 @@ n = 7
 const n = 7;
 
 const maxSpaces = Math.trunc(n / 2);
-console.log("maxSpaces: ", maxSpaces);
 
 // Max spaces string
 let spacesRow = "";
-for (let indexSpaces = maxSpaces; indexSpaces >= 0; indexSpaces -= 1) {
+for (let indexSpaces = maxSpaces; indexSpaces > 0; indexSpaces -= 1) {
   spacesRow += " ";
 }
 
@@ -30,6 +29,7 @@ for (let index = 1; index <= n; index += 2) {
     }
   }
 
-  spacesRow = spacesRow.replace(spacesRow[spacesRow.length - 1], "");
   console.log(spacesRow + row);
+
+  spacesRow = spacesRow.replace(spacesRow[spacesRow.length - 1], "");
 }
