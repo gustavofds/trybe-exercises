84066e12fdec94aa9075fe4851ee64cc1483b22e
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-06 13:12:35 -0300

    Exercicio 1 - adiciona days li

--- FUNDAMENTALS/BLOCO_5/DIA_3/script.js
+++ FUNDAMENTALS/BLOCO_5/DIA_3/script.js
@@ -19,6 +19,72 @@ function createDaysOfTheWeek() {
   }
 }
 
+function createDays() {
+  const dezDaysList = [
+    29,
+    30,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+  ];
+
+  const daysList = document.getElementById("days");
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const day = dezDaysList[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.className = "day";
+    dayListItem.innerText = day;
+
+    if (
+      dayListItem.innerHTML.includes(24) ||
+      dayListItem.innerHTML.includes(25) ||
+      dayListItem.innerHTML.includes(31)
+    ) {
+      dayListItem.className += " holiday";
+    }
+
+    if (
+      dayListItem.innerHTML.includes(4) ||
+      dayListItem.innerHTML.includes(11) ||
+      dayListItem.innerHTML.includes(18)
+    ) {
+      dayListItem.className += " friday";
+    }
+
+    daysList.append(dayListItem);
+  }
+}
+
 createDaysOfTheWeek();
+createDays();
 
 // Escreva seu código abaixo.
