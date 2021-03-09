cdb1160b912da851146e4de4c15303de52fe0b82
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-09 12:35:22 -0300

    Finalizacao do localStorage

--- FUNDAMENTALS/BLOCO_5/DIA_4/script.js
+++ FUNDAMENTALS/BLOCO_5/DIA_4/script.js
@@ -8,6 +8,22 @@ const inputFontFamily = document.getElementById('font-family');
 window.onload = function() {
   if (localStorage.backgroundColor) {
     document.body.style.backgroundColor = localStorage.backgroundColor;
+  } 
+
+  if (localStorage.color) {
+    document.body.style.color = localStorage.color;
+  }
+
+  if (localStorage.fontSize) {
+    document.body.style.fontSize = localStorage.fontSize;
+  }
+
+  if (localStorage.lineHeight) {
+    document.body.style.lineHeight = localStorage.lineHeight;
+  }
+
+  if (localStorage.fontFamily) {
+    document.body.style.fontFamily = localStorage.fontFamily;
   }
 }
 
@@ -18,19 +34,28 @@ function changeBackgroundColor() {
 
 function changeColor() {
   document.body.style.color = inputColor.value;
+  localStorage.color = inputColor.value;
 }
 
 function changeFontSize() {
   document.body.style.fontSize = inputFontSize.value;
+  localStorage.fontSize = inputFontSize.value;
 }
 
 function changeLineHeight() {
   document.body.style.lineHeight = inputLineHeight.value;
+  localStorage.lineHeight = inputLineHeight.value;
+}
+
+function changeFontFamily() {
+  document.body.style.fontFamily = inputFontFamily.value;
+  localStorage.fontFamily = inputFontFamily.value;
 }
 
 inputBackgroundColor.addEventListener('change', changeBackgroundColor);
 inputColor.addEventListener('change', changeColor);
 inputFontSize.addEventListener('change', changeFontSize);
 inputLineHeight.addEventListener('change', changeLineHeight);
+inputFontFamily.addEventListener('change', changeFontFamily);
 
 
