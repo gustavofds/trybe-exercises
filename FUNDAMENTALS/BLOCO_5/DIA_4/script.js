8eb85beed9c905f63bd107a636b75f4e3416aee6
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-09 12:27:45 -0300

    Mudanca de fontSize e lineHeight e inicio do localStoraget

--- FUNDAMENTALS/BLOCO_5/DIA_4/script.js
+++ FUNDAMENTALS/BLOCO_5/DIA_4/script.js
@@ -5,15 +5,32 @@ const inputFontSize = document.getElementById('font-size');
 const inputLineHeight = document.getElementById('line-height');
 const inputFontFamily = document.getElementById('font-family');
 
+window.onload = function() {
+  if (localStorage.backgroundColor) {
+    document.body.style.backgroundColor = localStorage.backgroundColor;
+  }
+}
+
 function changeBackgroundColor() {
   document.body.style.backgroundColor = inputBackgroundColor.value;
+  localStorage.backgroundColor = inputBackgroundColor.value;
 }
 
 function changeColor() {
   document.body.style.color = inputColor.value;
 }
 
+function changeFontSize() {
+  document.body.style.fontSize = inputFontSize.value;
+}
+
+function changeLineHeight() {
+  document.body.style.lineHeight = inputLineHeight.value;
+}
+
 inputBackgroundColor.addEventListener('change', changeBackgroundColor);
 inputColor.addEventListener('change', changeColor);
+inputFontSize.addEventListener('change', changeFontSize);
+inputLineHeight.addEventListener('change', changeLineHeight);
 
 
