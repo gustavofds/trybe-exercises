2af653aaa4fe135dc445bbc1b8c741d2208a5305
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-15 19:35:57 -0300

    Exercicio 1 - corrige state dropdown

--- FUNDAMENTALS/BLOCO_6/DIA_1/script.js
+++ FUNDAMENTALS/BLOCO_6/DIA_1/script.js
@@ -1,35 +1,40 @@
 const selectDropdown = document.getElementById('state');
 
-const statesOfBrazil = ['Acre', 
-  'Alagoas', 
-  'Amapá', 
-  'Amazonas',
-  'Bahia',
-  'Ceará',
-  'Distrito Federal',
-  'Espirito Santo',
-  'Goiás',
-  'Maranhão',
-  'Mato Grosso do Sul',
-  'Mato Grosso',
-  'Minas Gerais',
-  'Pará',
-  'Paraíba',
-  'Paraná',
-  'Pernambuco',
-  'Piauí',
-  'Rio de Janeiro',
-  'Rio Grande do Norte',
-  'Rio Grande do Sul',
-  'Rondônia',
-  'Roraima',
-  'Santa Catarina',
-  'São Paulo',
-  'Sergipe',
-  'Tocantins']
+const statesOfBrazil = [
+  { 'AC': 'Acre' },
+  { 'AL': 'Alagoas' },
+  { 'AP': 'Amapá' },
+  { 'AM': 'Amazonas' },
+  { 'BA': 'Bahia' },
+  { 'CE': 'Ceará' },
+  { 'DF': 'Distrito Federal' },
+  { 'ES': 'Espírito Santo' },
+  { 'GO': 'Goías' },
+  { 'MA': 'Maranhão' },
+  { 'MT': 'Mato Grosso' },
+  { 'MS': 'Mato Grosso do Sul' },
+  { 'MG': 'Minas Gerais' },
+  { 'PA': 'Pará' },
+  { 'PB': 'Paraíba' },
+  { 'PR': 'Paraná' },
+  { 'PE': 'Pernambuco' },
+  { 'PI': 'Piauí' },
+  { 'RJ': 'Rio de Janeiro' },
+  { 'RN': 'Rio Grande do Norte' },
+  { 'RS': 'Rio Grande do Sul' },
+  { 'RO': 'Rondônia' },
+  { 'RR': 'Roraíma' },
+  { 'SC': 'Santa Catarina' },
+  { 'SP': 'São Paulo' },
+  { 'SE': 'Sergipe' },
+  { 'TO': 'Tocantins' },
+]
 
 for (let index = 0; index < statesOfBrazil.length; index += 1) {
+  const optionValue = Object.keys(statesOfBrazil[index])[0];  
+  const optionText = statesOfBrazil[index][optionValue];  
   const optionTag = document.createElement('option');
-  optionTag.innerText = statesOfBrazil[index];
+  optionTag.innerText = optionText;
+  optionTag.setAttribute('value', optionValue);
   selectDropdown.appendChild(optionTag);
 }
