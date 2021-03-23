aaaa8930759f7922ad7cd57f794fdb8fc8145500
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-23 19:14:55 -0300

    Parte 2.1 - bonus: calcula fatorial recursivamente

--- FUNDAMENTALS/BLOCO_7/DIA_1/script2.js
+++ FUNDAMENTALS/BLOCO_7/DIA_1/script2.js
@@ -1,12 +1,17 @@
 // Parte II
 // 1 - Fatorial
-const calcFactorial = number => {
-  let factorial = number;
-  for (let index = number - 1; index >= 1; index -= 1) {
-    factorial *= index;
-  }
-  return factorial;
-}
+// const calcFactorial = number => {
+//   let factorial = number;
+//   for (let index = number - 1; index >= 1; index -= 1) {
+//     factorial *= index;
+//   }
+//   return factorial;
+// }
 
-console.log(calcFactorial(5));
-console.log(calcFactorial(4));
+const calcFactorial2 = number => number === 1 ? 1 : number * calcFactorial2(number - 1);
+
+console.log(calcFactorial2(5));
+console.log(calcFactorial2(4));
+console.log(calcFactorial2(11));
+
+// 2
