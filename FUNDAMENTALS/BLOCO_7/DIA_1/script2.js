5642a57b2f67c76dd103a52e41960c8584a752da
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-23 19:19:58 -0300

    Parte 2.2 - maior palavra de uma frase

--- FUNDAMENTALS/BLOCO_7/DIA_1/script2.js
+++ FUNDAMENTALS/BLOCO_7/DIA_1/script2.js
@@ -15,3 +15,15 @@ console.log(calcFactorial2(4));
 console.log(calcFactorial2(11));
 
 // 2
+const longestWord = phrase => {
+  const words = phrase.split(' ');
+  let longest = words[0];
+
+  for (let index = 1; index < words.length; index += 1) {
+    longest = longest.length < words[index].length ? words[index] : longest; 
+  }
+
+  return longest;
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")) // retorna 'aconteceu'
