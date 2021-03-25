410b622118423275d8958f3bf2b7f02a8f84497b
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-25 12:36:16 -0300

    Parte 2.1 - funcao que adiciona turno da manha

---
+++ FUNDAMENTALS/BLOCO_7/DIA_2/script2.js
@@ -0,0 +1,29 @@
+// Parte 2
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// 1 
+function addTurn(obj, newKey, value) {
+  obj[newKey] = value;
+  return obj;
+}
+
+console.log(addTurn(lesson2, 'turno', 'manhã'));
