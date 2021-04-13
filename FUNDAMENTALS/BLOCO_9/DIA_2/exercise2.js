31b17964aa96b695b6954ecca040d7a3ddc87d92
Gustavo Sousa <gustavohl98@gmail.com>
2021-04-13 19:03:54 -0300

    Exercicio 2 - encapsula a new Promise dentro de FetchPromise()

--- FUNDAMENTALS/BLOCO_9/DIA_2/exercise2.js
+++ FUNDAMENTALS/BLOCO_9/DIA_2/exercise2.js
@@ -2,19 +2,22 @@ function randomNumber() {
   return Math.floor(Math.random() * 50);
 }
 
-const promise = new Promise((resolve, reject) => {
-  const randomNumbers = [];
-  for (let index = 0; index < 5; index += 1) {
-    randomNumbers.push(randomNumber());
-  }
-  const randomNumbersSqrSum = randomNumbers.map((number) => number ** 2)
-    .reduce((acc, cur) => acc + cur, 0);
+const fetchPromise = () => {
+  const promise = new Promise((resolve, reject) => {
+    const randomNumbers = [];
+    for (let index = 0; index < 5; index += 1) {
+      randomNumbers.push(randomNumber());
+    }
+    const randomNumbersSqrSum = randomNumbers.map((number) => number ** 2)
+      .reduce((acc, cur) => acc + cur, 0);
 
-  if (randomNumbersSqrSum < 8000) return resolve();
-  reject();
-})
-  .then((response) => console.log('Promise resolvida'))
-  .catch((err) => console.log('Promise rejeitada'))
+    if (randomNumbersSqrSum < 8000) return resolve();
+    reject();
+  })
+    .then((response) => console.log('Promise resolvida'))
+    .catch((err) => console.log('Promise rejeitada'));
+}
 
+fetchPromise();
 
 
