9da301297d3743c8aff024559ef9d0e00b40753d
Gustavo Sousa <gustavohl98@gmail.com>
2021-04-20 13:24:27 -0300

    Parte 1 exercicio 2 - testa funcao myRemove

--- FUNDAMENTALS/BLOCO_10/DIA_1/exercises-p1.test.js
+++ FUNDAMENTALS/BLOCO_10/DIA_1/exercises-p1.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./exercises-pt1');
+const { sum, myRemove } = require('./exercises-pt1');
 
 describe('Exercicio 1 - testa sum', () => {
   test('Testa se sum 4 + 5 é igual a 9', () => {
@@ -16,5 +16,24 @@ describe('Exercicio 1 - testa sum', () => {
   test('Testa se a mensagem de erro é parameters must be numbers', () => {
     expect(() => { sum(4, '5') }).toThrow(new Error('parameters must be numbers'));
   });
-});
+})
+
+describe('Exercicio 2 - testa myRemove', () => {
+  it('Verifica se remove o 3', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('Verifica não retorna [1,2,3,4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('Verifica se o array passado por parâmetro não sofreu alterações', () => {
+    const testArr = [1, 2, 3, 4];
+    expect(myRemove(testArr, 3)).not.toEqual(testArr);
+  });
+
+  it('Verifica se retorna array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+})
 
