7e9163be9ee515caea44bfbc8d978cdbf86a0a5e
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-26 21:57:53 -0300

    Exercise 6 - Same query using SUBQUERIES and INNER JOIN

--- BACK_END/BLOCO_21/DIA_2/exercises-21-2.sql
+++ BACK_END/BLOCO_21/DIA_2/exercises-21-2.sql
@@ -31,3 +31,15 @@ FROM Pixar.Theater AS T
 RIGHT JOIN Pixar.Movies AS M
 ON M.theater_id = T.id
 ORDER BY T.name;
+
+-- 6
+SELECT title FROM Pixar.Movies
+WHERE id IN (
+	SELECT movie_id FROM Pixar.BoxOffice
+    WHERE rating > 7.5
+);
+
+SELECT M.title FROM Pixar.Movies AS M
+INNER JOIN Pixar.BoxOffice AS BO
+ON BO.movie_id = M.id
+WHERE BO.rating > 7.5;
