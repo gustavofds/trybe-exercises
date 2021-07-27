db7ee5a8c7a1415ffb5f75e44a23dc7dbabf2747
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-26 21:31:49 -0300

    Exercises 2 and 3 - INNER JOIN

--- BACK_END/BLOCO_21/DIA_2/exercises-21-2.sql
+++ BACK_END/BLOCO_21/DIA_2/exercises-21-2.sql
@@ -3,3 +3,17 @@ SELECT M.title, BO.domestic_sales, BO.international_sales
 FROM Pixar.Movies AS M
 INNER JOIN Pixar.BoxOffice AS BO
 ON BO.movie_id = M.id;
+
+-- 2
+SELECT M.title, BO.domestic_sales, BO.international_sales
+FROM Pixar.Movies AS M
+INNER JOIN Pixar.BoxOffice AS BO
+ON BO.movie_id = M.id
+WHERE BO.international_sales > BO.domestic_sales;
+
+-- 3
+SELECT M.title, BO.rating
+FROM Pixar.Movies AS M
+INNER JOIN Pixar.BoxOffice AS BO
+ON BO.movie_id = M.id
+ORDER BY BO.rating DESC;
