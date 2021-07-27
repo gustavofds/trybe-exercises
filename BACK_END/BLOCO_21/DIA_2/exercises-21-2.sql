dcc0a2f2b0b08f003fc50ee784fe903939d6c897
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-26 21:43:28 -0300

    Exercise 4 -LEFT JOIN

--- BACK_END/BLOCO_21/DIA_2/exercises-21-2.sql
+++ BACK_END/BLOCO_21/DIA_2/exercises-21-2.sql
@@ -17,3 +17,10 @@ FROM Pixar.Movies AS M
 INNER JOIN Pixar.BoxOffice AS BO
 ON BO.movie_id = M.id
 ORDER BY BO.rating DESC;
+
+-- 4
+SELECT T.*, M.*
+FROM Pixar.Theater AS T
+LEFT JOIN Pixar.Movies AS M
+ON M.theater_id = T.id
+ORDER BY T.name;
