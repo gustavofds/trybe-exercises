9b25b91f454658f7ff8acc2de9fc6fa35a763c49
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-27 19:36:50 -0300

    Exercise 2 - TRIGGER before update

--- BACK_END/BLOCO_21/DIA_3/exercises-21-3.sql
+++ BACK_END/BLOCO_21/DIA_3/exercises-21-3.sql
@@ -10,8 +10,6 @@ BEGIN
 END $$
 DELIMITER ;
 
-DROP TRIGGER IF EXISTS set_new_movie_log;
-
 DELIMITER $$
 CREATE TRIGGER set_new_movie_log
 	AFTER INSERT ON movies
@@ -19,5 +17,17 @@ CREATE TRIGGER set_new_movie_log
 BEGIN
 	INSERT INTO movies_logs (movie_id, executed_action, log_date)
     VALUES (NEW.movie_id, 'INSERT', CURRENT_DATE());
+END $$
+DELIMITER ;
+
+-- 2
+DELIMITER $$
+CREATE TRIGGER set_trigger_price_update
+	BEFORE UPDATE ON movies
+    FOR EACH ROW
+BEGIN
+	SET NEW.ticket_price_estimation = IF(NEW.ticket_price > OLD.ticket_price, 'Increasing', 'Decreasing');
+    INSERT INTO movies_logs (movie_id, executed_action, log_date)
+    VALUES (NEW.movie_id, 'UPDATE', NOW());
 END $$
 DELIMITER ;
