26fc9e5a54fb8e6473ef5d583033e28cb22a3997
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-28 10:07:23 -0300

    Exercise 3 - TRIGGER after DELETE

--- BACK_END/BLOCO_21/DIA_3/exercises-21-3.sql
+++ BACK_END/BLOCO_21/DIA_3/exercises-21-3.sql
@@ -31,3 +31,14 @@ BEGIN
     VALUES (NEW.movie_id, 'UPDATE', NOW());
 END $$
 DELIMITER ;
+
+-- 3
+DELIMITER $$
+CREATE TRIGGER set_delete_log
+	AFTER DELETE ON movies
+    FOR EACH ROW
+BEGIN
+	INSERT INTO movies_logs (movie_id, executed_action, log_date)
+    VALUES (OLD.movie_id, 'DELETE', CURRENT_DATE());
+END $$
+DELIMITER ;
