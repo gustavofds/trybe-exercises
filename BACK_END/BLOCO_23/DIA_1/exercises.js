8d216df4d28112d09fc7ea79fc6b1e8281774cd1
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-09 19:02:50 -0300

    Exercises 5 and 6 finished

--- BACK_END/BLOCO_23/DIA_1/exercises.js
+++ BACK_END/BLOCO_23/DIA_1/exercises.js
@@ -9,3 +9,11 @@ db.bios.find({ _id: 8 }, { name: 1, birth: 1, _id: 0 });
 
 // 4
 db.bios.find({ "name.first": "John"  }).pretty();
+
+// 5
+db.bios.find().limit(3).pretty();
+
+// 6
+db.bios.find().limit(2).skip(5);
+
+// 7
