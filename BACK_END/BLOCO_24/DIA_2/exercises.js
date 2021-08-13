b5793488167ec2be64ac4ee58f07fa90bc5014d3
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-13 19:58:23 -0300

    Exercise 3 - $pull

--- BACK_END/BLOCO_24/DIA_2/exercises.js
+++ BACK_END/BLOCO_24/DIA_2/exercises.js
@@ -9,3 +9,9 @@ db.movies.updateOne(
   { title: "Batman" },
   { $push: { category: { $each: ["villain", "comic-based"] } } }
 );
+
+// 3
+db.movies.updateOne(
+  { title: "Batman" },
+  { $pull: { category: "action" }}
+);
