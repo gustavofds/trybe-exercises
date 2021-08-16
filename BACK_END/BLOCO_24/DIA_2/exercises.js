dec684a5b5de89525792c95794146c5fdefa0748
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-16 11:10:48 -0300

    Exercise 10 - $push + $each

--- BACK_END/BLOCO_24/DIA_2/exercises.js
+++ BACK_END/BLOCO_24/DIA_2/exercises.js
@@ -80,3 +80,24 @@ db.movies.updateOne(
   { title: "Home Alone", "cast.actor": "Daniel Stern" },
   { $set: { "cast.$.character": "Marv" } }
 );
+
+// 10
+db.movies.updateOne(
+  { title: "Batman" },
+  { $push: {
+      cast: {
+        $each: [
+          {
+            "character": "Batman"
+          },
+          {
+            "character": "Alfred"
+          },
+          {
+            "character": "Coringa"
+          }
+        ],
+      },
+    },
+  },
+);
