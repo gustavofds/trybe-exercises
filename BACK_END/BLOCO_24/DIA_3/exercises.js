2d8de8750a85a07c8953b042817f883d868018ab
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-17 09:41:21 -0300

    Exercises 2~3 finished

--- BACK_END/BLOCO_24/DIA_3/exercises.js
+++ BACK_END/BLOCO_24/DIA_3/exercises.js
@@ -2,3 +2,20 @@
 db.movies.find(
   { category: { $all: ["action", "adventure"] } }
 ).pretty();
+
+// 2
+db.movies.find(
+  {
+    category: { $all: ["action"] },
+    imdbRating: { $gt: 7 }
+  }
+).pretty()
+
+// 3
+db.movies.updateOne(
+  { title: "Batman" },
+  {
+    $push: { ratings: { $each: [85, 100, 102, 105] }}
+  },
+)
+
