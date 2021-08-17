5dcbf705fc4055da79a361f09c336784c535672a
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-17 12:58:50 -0300

    Exercises 10 and 11: $size

--- BACK_END/BLOCO_24/DIA_3/exercises.js
+++ BACK_END/BLOCO_24/DIA_3/exercises.js
@@ -66,3 +66,9 @@ db.movies.find(
   },
   { title: 1, ratings: 1, category: 1, _id: 0}
 );
+
+// 10 
+db.movies.find({ category: { $size: 2 }}, { title: 1, _id: 0 });
+
+// 11
+db.movies.find({ ratings: { $size: 4 }}, { title: 1, _id: 0});
