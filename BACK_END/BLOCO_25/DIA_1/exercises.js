6184e6f33cbf22087989149c5760457036cb99e9
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-19 20:33:55 -0300

    Exercises 4~5: $group

--- BACK_END/BLOCO_25/DIA_1/exercises.js
+++ BACK_END/BLOCO_25/DIA_1/exercises.js
@@ -29,3 +29,14 @@ db.clientes.aggregate([
   },
   { $limit: 5 }
 ]);
+
+// 4
+db.clientes.aggregate([
+  { $match: { "endereco.uf": "SC" } },
+  { $group: { _id: "$endereco.uf", total: { $sum: 1 }  }  },
+]);
+
+// 5
+db.clientes.aggregate([
+  { $group: { _id: "$sexo", total: { $sum: 1 } } },
+]);
