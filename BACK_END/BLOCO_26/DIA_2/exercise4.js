43c026ad6fb9d073430106dc6cdb7949ae45af6c
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-25 22:33:44 -0300

    Exercise 4.2 - get simpson by id using promises

--- BACK_END/BLOCO_26/DIA_2/exercise4.js
+++ BACK_END/BLOCO_26/DIA_2/exercise4.js
@@ -2,9 +2,44 @@ const fs = require('fs').promises;
 
 const readArrOfObjects = async function (path) {
   const data = await fs.readFile(path, 'utf8');
-  const dataObj = JSON.parse(data);
+  const dataArr = JSON.parse(data);
   
-  dataObj.forEach((char) => console.log(`${char.id} - ${char.name}`))
+  dataArr.forEach((char) => console.log(`${char.id} - ${char.name}`))
 }
 
-readArrOfObjects('simpsons.json');
+// readArrOfObjects('simpsons.json');
+
+// 4.2
+const getSimpsonById = async function(id) {
+  try {
+    const data = await fs.readFile('simpsons.json', 'utf-8');
+    const dataArr = JSON.parse(data);
+
+    const selectedCharacter = dataArr.find((char) => +char.id === id);
+
+    if (!selectedCharacter) throw new Error('id não encontrado');
+
+    return selectedCharacter;
+  } catch(err) {
+    throw err;
+  }
+};
+
+(async () => {
+  try {
+    const data = await getSimpsonById(3);
+    console.log(data);
+    // console.log(await getSimpsonById(33))
+  } catch(err) {
+    console.log(err);
+  }
+})(); 
+
+(async () => {
+  try {
+    const data = await getSimpsonById(33);
+    console.log(data);
+  } catch(err) {
+    console.log('🤯 Error: ', err);  
+  }
+})(); 
