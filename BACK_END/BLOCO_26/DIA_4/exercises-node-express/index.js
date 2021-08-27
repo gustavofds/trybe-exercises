925643f4a494598d06c355d38024adbae837e2d1
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-27 19:23:38 -0300

    Exercises 5 and 6 - initiating Simpsons API + get route /simpsons

--- BACK_END/BLOCO_26/DIA_4/exercises-node-express/index.js
+++ BACK_END/BLOCO_26/DIA_4/exercises-node-express/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -23,6 +24,13 @@ app.post('/greetings', (req, res) => {
 app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
   res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+// Exercises 5~8
+const simpsons = JSON.parse(fs.readFileSync('simpsons.json', 'utf-8'));
+
+app.get('/simpsons', (_req, res) => {
+  res.status(200).json({ simpsonsQty: simpsons.length, simpsons });
 });
 
 app.listen(3000, () => {
