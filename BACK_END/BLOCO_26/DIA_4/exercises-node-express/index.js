01127f808fa04779e1eb0f4b9d350e43d1c69cea
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-27 19:07:53 -0300

    Exercise 2 - create post route /hello

--- BACK_END/BLOCO_26/DIA_4/exercises-node-express/index.js
+++ BACK_END/BLOCO_26/DIA_4/exercises-node-express/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong'});
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}!` })
 })
 
 app.listen(3000, () => {
