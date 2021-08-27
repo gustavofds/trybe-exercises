7c51c7a4559bc1821f6822df07df61424c1e1bfd
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-27 19:10:24 -0300

    Exercise 3 - create POST route /greetings

--- BACK_END/BLOCO_26/DIA_4/exercises-node-express/index.js
+++ BACK_END/BLOCO_26/DIA_4/exercises-node-express/index.js
@@ -11,7 +11,14 @@ app.get('/ping', (_req, res) => {
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(201).json({ message: `Hello, ${name}!` })
-})
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age <= 17) return res.status(401).json({ message: 'Unauthorized' });
+  res.status(200).json({ message: `Hello, ${name}!`});
+});
 
 app.listen(3000, () => {
   console.log('App running on port 3000!');
