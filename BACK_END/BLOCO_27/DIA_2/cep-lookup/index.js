08892dd30d6b4f6a7ec7132fc3ae420077405d91
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-07 17:38:14 -0300

    Creates test route GET /ping

---
+++ BACK_END/BLOCO_27/DIA_2/cep-lookup/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong!' });
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'));
