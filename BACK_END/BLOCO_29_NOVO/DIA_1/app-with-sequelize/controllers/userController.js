9cdc1e7d145d4fa3a45f67d9732e3d83852e26dc
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-07 15:27:38 -0300

    Exercicios de fixacao - implementa userController

---
+++ BACK_END/BLOCO_29_NOVO/DIA_1/app-with-sequelize/controllers/userController.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { User } = require('../models');
+const router = express.Router();
+
+// Este endpoint usa o método findAll do Sequelize para retorno todos os users.
+router.get('/', async (_req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+module.exports = router;
