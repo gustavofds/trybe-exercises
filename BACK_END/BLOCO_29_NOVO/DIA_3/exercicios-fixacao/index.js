20f9d053c9c05343b1301c1de9487e0d523b8d75
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-08 15:33:50 -0300

    Adiciona managed transaction

--- BACK_END/BLOCO_29_NOVO/DIA_3/exercicios-fixacao/index.js
+++ BACK_END/BLOCO_29_NOVO/DIA_3/exercicios-fixacao/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const config = require('./config/config');
 const { Address, Employee, Book, User } = require('./models');
 
+const sequelize = new Sequelize(config.development);
+
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -35,6 +41,33 @@ app.get('/employees/:id', async (req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   };
+});
+
+app.post('/employees', async (req, res) => {
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const result = await sequelize.transaction(async (t) => {
+      const employee = await Employee.create({
+        firstName, lastName, age
+      }, { transaction: t });
+
+      await Address.create({
+        city, street, number, employeeId: employee.id
+      }, { transaction: t });
+
+      return res.status(201).json({ message: 'Cadastrado com sucesso' });
+    });
+
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 app.get('/usersbooks/:id', async (req, res) => {
