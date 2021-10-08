381b8447f705862b7fc342ac97c9c4599c3d4423
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-08 13:00:20 -0300

    Corrige model Address

--- BACK_END/BLOCO_29_NOVO/DIA_3/exercicios-fixacao/models/Address.js
+++ BACK_END/BLOCO_29_NOVO/DIA_3/exercicios-fixacao/models/Address.js
@@ -1,6 +1,22 @@
-Employee.associate = (models) => {
-  Employee.hasOne(models.Address,
-    { foreignKey: 'employee_id', as: 'addresses' });
-};
+module.exports = (sequelize, DataTypes) => {
+  const Address = sequelize.define('Address', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    city: DataTypes.STRING,
+    street: DataTypes.STRING,
+    number: DataTypes.INTEGER,
+    employeeId: { type: DataTypes.INTEGER, foreignKey: true },
+    // A declaração da Foreign Key é opcional no model
+  },
+  {
+    timestamps: false,
+    tableName: 'Addresses',
+    underscored: true,
+  });
 
-return Employee;
+  Address.associate = (models) => {
+    Address.belongsTo(models.Employee,
+      { foreignKey: 'employee_id', as: 'employees' });
+  };
+
+  return Address;
+};
