be7b5c48a3a8f2e1d9c86b85fcf3bef71001c48e
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-20 13:21:38 -0300

    Pequena alteracao no ejs

--- BACK_END/BLOCO_30/DIA_1/mvc-example/controllers/authorController.js
+++ BACK_END/BLOCO_30/DIA_1/mvc-example/controllers/authorController.js
@@ -3,7 +3,7 @@ const Author = require('../models/Author');
 const listAuthors = async (req, res) => {
   const authors = await Author.getAll();
 
-  res.status(200).render('authors/index', { authors });
+  res.status(200).render('authors/index', { authors, me: 'Gustavo Sousa' });
 };
 
 module.exports = {
