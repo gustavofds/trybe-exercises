794c420ce907e2f761ed36dda250d797f1d8522b
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-20 15:25:52 -0300

    Implementa rota POST /authors

--- BACK_END/BLOCO_30/DIA_1/mvc-example/index.js
+++ BACK_END/BLOCO_30/DIA_1/mvc-example/index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 dotenv.config();
 
 const authorController = require('./controllers/authorController');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.set('views', './views');
 
 app.get('/authors', authorController.listAuthors);
+app.get('/authors/new', authorController.newAuthor);
+
 app.get('/authors/:id', authorController.showAuthor);
+app.post('/authors', authorController.createAuthor);
 
 const PORT = process.env.PORT || 3000;
 
