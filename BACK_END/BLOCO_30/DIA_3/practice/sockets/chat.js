71fb1ec9bd6e6fa4044e49e673879fe127788ec0
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-22 15:41:40 -0300

    socket.emit vs io.emit e socket.broadcast.emit

--- BACK_END/BLOCO_30/DIA_3/practice/sockets/chat.js
+++ BACK_END/BLOCO_30/DIA_3/practice/sockets/chat.js
@@ -1,5 +1,8 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    socket.emit('serverMessage', 'Olá, seja bem vindo ao nosso chat público! Use essa página para conversar a vontade.');
+    socket.broadcast.emit('serverMessage', `Irrrrrraaa! ${socket.id} acabou de se conectar :D`);
+
     socket.on('clientMessage', (message) => {
       console.log(`Mensagem ${message}`);
 
