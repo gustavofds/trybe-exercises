a95e0fbb0d27ceb61dc6cd336748451e341ea4ad
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-22 13:39:47 -0300

    Cria e escuta evento pong

--- BACK_END/BLOCO_30/DIA_3/practice/index.js
+++ BACK_END/BLOCO_30/DIA_3/practice/index.js
@@ -14,6 +14,7 @@ io.on('connection', (socket) => {
 
   socket.on('ping', () => {
     console.log(`${socket.id} emitiu um ping!`);
+    io.emit('pong', `${socket.id} enviou um ping!`);
   });
 });
 
